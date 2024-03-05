ef7b968c1c3de5d9e497062e43247edbc47348cf
Popa Vlad <popa.vlad94@gmail.com>
2024-03-05 11:35:42 +0200

    add meta to all the pages

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,12 @@
 <script setup></script>
 <template>
+    <Html>
+        <Head>
+            <Title>Emergency Locksmith Service Near Me | LocalLocksmith4All!</Title>
+            <Meta name="description" content="`24/7 emergency locksmith services. Locksmith4All also provide a range of services and supplies designed to improve the security of your home or business`" />
+            <Link src="//scripts.iconnode.com/95446.js" />
+        </Head>
+    </Html>
     <NuxtLayout>
         <HeroContent />
         <BadgesContent />
