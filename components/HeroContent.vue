1bac9c5a5e42781fa976c3128fd2d3c54ce43f63
Popa Vlad <popa.vlad94@gmail.com>
2024-03-08 15:26:20 +0200

    test

--- components/HeroContent.vue
+++ components/HeroContent.vue
@@ -28,6 +28,9 @@
                 <li class="text-white text-start pl-2">Quick & Convenient</li>
                 <li class="text-white text-start pl-2">Local expertise</li>
                 <li class="text-white text-start pl-2">Satisfaction guarantee</li>
+                <li class="text-white text-start pl-2">Satisfaction guarantee</li>
+                <li class="text-white text-start pl-2">Satisfaction guarantee</li>
+                <li class="text-white text-start pl-2">Satisfaction guarantee</li>
                 <li class="text-white text-start pl-2 pb-2">Expert security consultationse</li>
               </ul>
             </div>
