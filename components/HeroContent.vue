4f8bdd3b4f66faa8943dfb9a3fbb394d72e202e2
Popa Vlad <popa.vlad94@gmail.com>
2024-03-09 13:41:57 +0200

    HeroContent refactor

--- components/HeroContent.vue
+++ components/HeroContent.vue
@@ -3,37 +3,36 @@
 <template>
   <div class="hero">
     <div class="container">
-      <div class="row d-flex flex-column-reverse flex-lg-row">
-        <div class="col-12 col-lg-6 d-flex hero-man"></div>
-        <div class="col-12 col-lg-6 d-flex align-self-center flex-column mt-md-0 pb-3">
-          <h1 class="text-white text-start"><span class="my-yellow">Local Locksmith</span> near me</h1>
-          <ul class="with-border bg-black">
-            <li class="text-white text-start pl-2 pt-2">Service on spot within 25 minutes</li>
-            <li class="text-white text-start pl-2">Local service from £39</li>
-            <li class="text-white text-start pl-2">Locks from £25</li>
-            <li class="text-white text-start pl-2">24/7 Local Emergency</li>
-            <li class="text-white text-start pl-2">Fully equipped locksmiths</li>
-            <li class="text-white text-start pl-2">No Call Out Charge</li>
-            <li class="text-white text-start pl-2">1 Year Warranty</li>
+      <div class="row flex-column-reverse flex-lg-row">
+        <div class="col-12 col-lg-6 hero-man"></div>
+        <div class="col-12 col-lg-6 pb-3">
+          <h1>Local Locksmith near me</h1>
+          <ul class="bg-black">
+            <li class="pt-2">Service on spot within 25 minutes</li>
+            <li>Local service from £39</li>
+            <li>Locks from £25</li>
+            <li>24/7 Local Emergency</li>
+            <li>Fully equipped locksmiths</li>
+            <li>No Call Out Charge</li>
+            <li>1 Year Warranty</li>
             <div class="d-md-none d-sm-block">
               <ul class="p-0">
-                <li class="text-white text-start pl-2">100% Satisfaction Guarantee</li>
-                <li class="text-white text-start pl-2">Local Locksmiths</li>
-                <li class="text-white text-start pl-2">Mobile service, we come to you</li>
-                <li class="text-white text-start pl-2">Licensed & insured professionals.</li>
-                <li class="text-white text-start pl-2">24/7 availability, for your peace of mind</li>
-                <li class="text-white text-start pl-2">Fast & efficient</li>
-                <li class="text-white text-start pl-2">Trustworthy</li>
-                <li class="text-white text-start pl-2">Reliable & Secure</li>
-                <li class="text-white text-start pl-2">Quick & Convenient</li>
-                <li class="text-white text-start pl-2 pb-2">Local expertise</li>
-                <li class="text-white text-start pl-2">Satisfaction guarantee</li>
-                <li class="text-white text-start pl-2 pb-2">Expert security consultationse</li>
+                <li>100% Satisfaction Guarantee</li>
+                <li>Local Locksmiths</li>
+                <li>Mobile service, we come to you</li>
+                <li>Licensed & insured professionals.</li>
+                <li>24/7 availability, for your peace of mind</li>
+                <li>Fast & efficient</li>
+                <li>Trustworthy</li>
+                <li>Reliable & Secure</li>
+                <li>Quick & Convenient</li>
+                <li>Local expertise</li>
+                <li>Satisfaction guarantee</li>
+                <li class="pb-2">Expert security consultationse</li>
               </ul>
             </div>
-            
           </ul>
-          <button class="btn btn-primary w-100 my-primary-btn mt-1">
+          <button class="btn btn-primary w-100 mt-1">
             <a class="text-white text-decoration-none" href="tel:01173256595">At your location within 25 minutes</a>
           </button>
         </div>
@@ -47,7 +46,6 @@
   background-image: url("@/assets/images/hero-background.webp");
   background-size: cover;
   background-repeat: no-repeat;
-  max-height: 500px;
   border-bottom: 10px solid #000000;
 
   @media (max-width: 700px) {
@@ -55,6 +53,7 @@
     background-image: none;
     background-color: #000000;
   }
+
   .hero-man {
     background-image: url("@/assets/images/header-man.webp");
     background-size: contain;
@@ -68,17 +67,12 @@
   li {
     font-family: 'Ubuntu', sans-serif;
     list-style: disc;
+    color: #fff;
   }
 }
 h1 {
   font-size: 2.5rem;
-  &.with-border {
-    border-left: 10px solid #B30000;
-    padding-left: 25px;
-    @media (max-width: 700px) {
-      font-size: 3.5rem;
-    }
-  }
+  color: #fff;
 }
 .btn-primary {
   background-color: #B30000 !important;
@@ -86,7 +80,6 @@ h1 {
   &:focus, :active, :target, :visited {
     border: none;
     box-shadow: none;
-
   }
   a {
     text-align: center;
